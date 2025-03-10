d09fe58ed839c689f52ec697cacba957ecd17b83
Djall1lrml <abdeldjallilramoul23@gmail.com>
2025-03-10 15:55:08 +0100

    Delete log_script.js

--- log_script.js
+++
@@ -1,6 +0,0 @@
-const boxo=document.querySelector(boxo);
-const registerbtn=document.querySelector(btn-reg);
-const loginbtn=document.querySelector(btn-log);
-registerbtn.addEventListener('click',() =>{
-    boxo.classlist.add('active');
-});
