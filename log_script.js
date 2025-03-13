e0a2a5ecdf7145f4e632bb690862a8600c714786
Anis-Dje <anisleprince2005@gmail.com>
2025-03-13 15:04:20 +0100

    clean branch

--- log_script.js
+++
@@ -1,9 +0,0 @@
-const stform = document.querySelector('.stform');
-const registerbtn = document.querySelector('.btn-reg');
-const loginbtn = document.querySelector('.btn-log');
-registerbtn.addEventListener('click', () => {
-    stform.classList.add('active');
-});
-loginbtn.addEventListener('click', () => {
-    stform.classList.remove('active');
-});
